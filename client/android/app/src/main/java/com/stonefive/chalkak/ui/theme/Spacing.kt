8189a748238bb102e73e5aed39f650ef29dc9039
정찬 <chan679999@naver.com>
2026-08-11 13:18:57 +0900

    feat: 안드로이드 테마 설정

---
+++ client/android/app/src/main/java/com/stonefive/chalkak/ui/theme/Spacing.kt
@@ -0,0 +1,28 @@
+package com.stonefive.chalkak.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Immutable
+data class ChalkakSpacing(
+    val none: Dp,
+    val xs: Dp,
+    val sm: Dp,
+    val md: Dp,
+    val lg: Dp,
+    val xl: Dp,
+    val xxl: Dp,
+    val screenHorizontal: Dp,
+)
+
+val DefaultChalkakSpacing = ChalkakSpacing(
+    none = 0.dp,
+    xs = 4.dp,
+    sm = 8.dp,
+    md = 12.dp,
+    lg = 16.dp,
+    xl = 24.dp,
+    xxl = 32.dp,
+    screenHorizontal = 20.dp,
+)
