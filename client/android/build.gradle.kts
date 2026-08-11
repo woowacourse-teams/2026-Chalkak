bda41f0f1993d7e19dfaa0222c462b61d807ade2
정찬 <chan679999@naver.com>
2026-08-11 14:40:38 +0900

    fix: ktlint 루트 태스크에 app 검사 연결

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -1,7 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    // 루트 프로젝트에도 적용해야 Git pre-commit 훅 생성 태스크가 등록됩니다.
     alias(libs.plugins.ktlint)
+}
+
+tasks.named("ktlintCheck") {
+    dependsOn(":app:ktlintCheck")
+}
+
+tasks.named("ktlintFormat") {
+    dependsOn(":app:ktlintFormat")
 }
