cd078289d40c125064c1bda10ca32bc03d8e9eb6
정찬 <chan679999@naver.com>
2026-08-20 21:42:44 +0900

    refactor: 설정 스크린 스크롤 기능 추가

--- client/android/app/src/main/java/com/stonefive/chalkak/feature/settings/SettingsScreen.kt
+++ client/android/app/src/main/java/com/stonefive/chalkak/feature/settings/SettingsScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -87,7 +89,8 @@ fun SettingsScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(horizontal = ChalkakTheme.spacing.screenHorizontal),
+                .padding(horizontal = ChalkakTheme.spacing.screenHorizontal)
+                .verticalScroll(rememberScrollState()),
         ) {
             Spacer(modifier = Modifier.height(48.dp))
 
