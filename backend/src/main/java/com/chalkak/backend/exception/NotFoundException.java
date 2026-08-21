9a89c1538ce3e2b52645eebc390ec79213ff5dc6
smiinii <sm020320@naver.com>
2026-08-21 13:30:22 +0900

    fix: 공통 예외 생성자 접근 제어자 변경 (#75)

--- backend/src/main/java/com/chalkak/backend/exception/NotFoundException.java
+++ backend/src/main/java/com/chalkak/backend/exception/NotFoundException.java
@@ -2,7 +2,7 @@ package com.chalkak.backend.exception;
 
 public class NotFoundException extends BaseException {
 
-    protected NotFoundException(ErrorCode errorCode, String message) {
+    public NotFoundException(ErrorCode errorCode, String message) {
         super(errorCode, message);
     }
 }
