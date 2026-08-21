31a96ab272d9573f9a883737af687a98bc00f1b8
smiinii <sm020320@naver.com>
2026-08-21 13:04:11 +0900

    fix: 공통 예외 생성자 접근 제어자 변경

--- backend/src/main/java/com/chalkak/backend/exception/BusinessException.java
+++ backend/src/main/java/com/chalkak/backend/exception/BusinessException.java
@@ -2,7 +2,7 @@ package com.chalkak.backend.exception;
 
 public class BusinessException extends BaseException {
 
-    protected BusinessException(ErrorCode errorCode, String message) {
+    public BusinessException(ErrorCode errorCode, String message) {
         super(errorCode, message);
     }
 }
