e75676843ee048e49dcfc07f33223446d335215d
cbnuLeehyunwoo <henow123@naver.com>
2026-08-22 23:34:53 +0900

    refactor: 삭제버튼 타이포그래피 변경

--- client/android/app/src/main/java/com/stonefive/chalkak/feature/feed/component/FeedTopBar.kt
+++ client/android/app/src/main/java/com/stonefive/chalkak/feature/feed/component/FeedTopBar.kt
@@ -67,7 +67,7 @@ fun FeedTopBar(
             Text(
                 text = "삭제",
                 color = ChalkakTheme.colors.destructive,
-                style = ChalkakTheme.typography.body,
+                style = ChalkakTheme.typography.callout,
             )
         }
     }
