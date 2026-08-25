730363bd08b33b31084aadca6330cfbdcab6c92c
cbnuLeehyunwoo <henow123@naver.com>
2026-08-25 16:40:51 +0900

    fix: 접힘 영역이 상태바를 침범하지 않도록 수정

--- client/android/app/src/main/java/com/stonefive/chalkak/feature/home/HomeScreen.kt
+++ client/android/app/src/main/java/com/stonefive/chalkak/feature/home/HomeScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
@@ -164,6 +165,7 @@ fun HomeScreen(
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
+                        .clipToBounds()
                         .collapsingTopArea(topAreaOffset),
                 ) {
                     Column(
