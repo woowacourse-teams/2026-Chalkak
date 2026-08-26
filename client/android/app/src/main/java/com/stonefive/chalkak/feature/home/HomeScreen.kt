3252d16ecfb51579417d06281b555b2c5d0cfb25
cbnuLeehyunwoo <henow123@naver.com>
2026-08-27 00:28:29 +0900

    refactor: 상태바 클릭 시 맨 위로 이동하는 기능 제거

--- client/android/app/src/main/java/com/stonefive/chalkak/feature/home/HomeScreen.kt
+++ client/android/app/src/main/java/com/stonefive/chalkak/feature/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.stonefive.chalkak.feature.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -170,12 +169,7 @@ fun HomeScreen(
             Spacer(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .windowInsetsTopHeight(WindowInsets.statusBars)
-                    .clickable(
-                        interactionSource = null,
-                        indication = null,
-                        onClick = { localResetSignal++ },
-                    ),
+                    .windowInsetsTopHeight(WindowInsets.statusBars),
             )
             HomeTopBar(
                 modifier = Modifier
