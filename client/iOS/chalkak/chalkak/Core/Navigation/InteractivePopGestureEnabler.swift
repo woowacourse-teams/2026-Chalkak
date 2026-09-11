b3a7ac19054ea4056e4835d8c42f69240ea39d60
정찬 <chan679999@naver.com>
2026-09-11 17:09:52 +0900

    refactor: delegate를 사용하도록 코드 수정

--- client/iOS/chalkak/chalkak/Core/Navigation/InteractivePopGestureEnabler.swift
+++ client/iOS/chalkak/chalkak/Core/Navigation/InteractivePopGestureEnabler.swift
@@ -13,12 +13,30 @@ struct InteractivePopGestureEnabler: UIViewControllerRepresentable {
 
         viewController.enableInteractivePopGesture()
     }
+
+    static func dismantleUIViewController(
+        _ uiViewController: UIViewController,
+        coordinator: ()
+    ) {
+        (uiViewController as? InteractivePopGestureViewController)?
+            .restoreInteractivePopGesture()
+    }
 }
 
 private final class InteractivePopGestureViewController: UIViewController {
+    private let interactivePopDelegate = InteractivePopGestureDelegate()
+    private weak var installedGestureRecognizer: UIGestureRecognizer?
+    private var originalDelegate: UIGestureRecognizerDelegate?
+    private var originalIsEnabled = false
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         enableInteractivePopGesture()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        restoreInteractivePopGesture()
     }
 
     func enableInteractivePopGesture() {
@@ -31,8 +49,41 @@ private final class InteractivePopGestureViewController: UIViewController {
                 return
             }
 
-            gestureRecognizer.delegate = nil
+            guard self.installedGestureRecognizer !== gestureRecognizer else {
+                gestureRecognizer.isEnabled = true
+                return
+            }
+
+            self.originalDelegate = gestureRecognizer.delegate
+            self.originalIsEnabled = gestureRecognizer.isEnabled
+            self.installedGestureRecognizer = gestureRecognizer
+            self.interactivePopDelegate.navigationController = navigationController
+            gestureRecognizer.delegate = self.interactivePopDelegate
             gestureRecognizer.isEnabled = true
         }
+    }
+
+    func restoreInteractivePopGesture() {
+        guard let gestureRecognizer = installedGestureRecognizer else { return }
+
+        if gestureRecognizer.delegate === interactivePopDelegate {
+            gestureRecognizer.delegate = originalDelegate
+            gestureRecognizer.isEnabled = originalIsEnabled
+        }
+
+        installedGestureRecognizer = nil
+        originalDelegate = nil
+        interactivePopDelegate.navigationController = nil
+    }
+}
+
+private final class InteractivePopGestureDelegate: NSObject, UIGestureRecognizerDelegate {
+    weak var navigationController: UINavigationController?
+
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let navigationController else { return false }
+
+        return navigationController.viewControllers.count > 1
+            && navigationController.transitionCoordinator == nil
     }
 }
