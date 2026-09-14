a572a89245940da8036ed626c087bf79b2231202
정찬 <chan679999@naver.com>
2026-09-14 16:28:59 +0900

    안드로이드 사진 로드 전 크기 조정(홈 스크린)

--- client/android/app/src/androidTest/java/com/stonefive/chalkak/feature/home/HomeScreenTest.kt
+++ client/android/app/src/androidTest/java/com/stonefive/chalkak/feature/home/HomeScreenTest.kt
@@ -93,6 +93,20 @@ class HomeScreenTest {
     }
 
     @Test
+    fun homePhotoUsesLoadedImageAspectRatio() {
+        setHomeContent(uiState = contentUiState())
+
+        composeRule.waitUntil {
+            val bounds = composeRule
+                .onNodeWithContentDescription("작품 이미지: 사진 0")
+                .fetchSemanticsNode()
+                .boundsInRoot
+            val aspectRatio = bounds.width / bounds.height
+            kotlin.math.abs(aspectRatio - 0.75f) < 0.02f
+        }
+    }
+
+    @Test
     fun genericErrorRefreshButtonIsAccessibleAndDispatchesRetry() {
         val actions = mutableListOf<HomeUiAction>()
         setHomeContent(
